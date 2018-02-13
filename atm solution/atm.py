55e53933199e21cc424d817302e310948a913079
Muhammed Yaser YAGAN <mhyasery@gmail.com>
2018-02-13 18:02:40 +0300

    ATMs using classes

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,28 +1,48 @@
-money = 500
-request = 499
-if request > money :
-    print 'There is no enough money in your account!'
-else :
-    while request > 0 :
-        if request >= 100 :
-            print 'Give 100'
-            request -= 100
-            money -= 100
-        elif request >= 50 :
-            print 'Give 50'
-            request -= 50
-            money -= 50
-        elif request >= 10 :
-            print 'Give 10'
-            request -= 10
-            money -= 10
-        elif request >= 5 :
-            print 'Give 5'
-            request -= 5
-            money -= 5
-        else :
-            print 'Give ', request
-            request = 0
-            money -= request
-    print 'You have ', money ,' in your account'
+class ATM:
+    def __init__(self,balance,bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
 
+    def withdraw(self,request):
+        print "Welcome to ", self.bank_name
+        print "Current balance = ",self.balance
+        print "=================================="
+        if   request > self.balance:
+            print("Can't give you all this money !!")
+            print "=================================="
+        elif request < 0:
+            print("More than zero plz!")
+            print "=================================="
+        else:
+            r = request
+            while request > 0:
+                if request >= 100:
+                    request -= 100
+                    print("give 100")
+                elif request >= 50:
+                    request -= 50
+                    print("give 50")
+                elif request >= 10:
+                    request -= 10
+                    print("give 10")
+                elif request >= 5:
+                    request -= 5
+                    print("give 5")
+                elif request < 5:
+                    print("give " + str(request))
+                    request = 0
+            print "=================================="
+            self.balance = self.balance - r
+
+
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
