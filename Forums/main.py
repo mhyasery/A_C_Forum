d879fb600a57534a862e1dc54577d6426f4d6fe3
Muhammed Yaser YAGAN <mhyasery@gmail.com>
2018-02-17 14:21:16 +0300

    Forums Entities

---
+++ Forums/main.py
@@ -0,0 +1,7 @@
+import Forums.models as classes
+
+member1 = classes.Member('Member1',22)
+member2 = classes.Member('Member2',29)
+
+post1 = classes.Post('Subject1','text1...')
+post2 = classes.Post('Subject2','text2...')
---
+++ Forums/models.py
@@ -0,0 +1,10 @@
+class Member:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+class Post:
+    def __init__(self,subject,text):
+        self.subject = subject
+        self.text = text
+
